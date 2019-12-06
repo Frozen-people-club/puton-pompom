37a2c928b9441d5944a3c53ad6ce3a0adf6e7084
Jiglionero <smouk.chayz@gmail.com>
2019-12-06 16:59:45 +0300

    create design branch

--- flask_backend/__init__.py
+++ flask_backend/__init__.py
@@ -6,7 +6,7 @@ from flask_backend.data import data_node as node
 import flask_backend.data
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = '../res'
+app.config['UPLOAD_FOLDER'] = '../res/public'
 data.init_app(app)
 
 
