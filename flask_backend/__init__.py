5fbd73c75cdb90bd5eaaa9bcacd894139c6065e5
Jiglionero <smouk.chayz@gmail.com>
2019-12-08 18:51:22 +0300

    add cors

--- flask_backend/__init__.py
+++ flask_backend/__init__.py
@@ -2,25 +2,29 @@
 from flask import Flask
 from flask import request
 from flask import send_from_directory
+from flask_cors import CORS, cross_origin
 from flask_backend.data import data_node as node
-import flask_backend.data
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = '../res'
+CORS(app)
 data.init_app(app)
 
 
 @app.route('/api/v1.0/current')
+@crossdomain(origin='*')
 def current():
     return node.get_current_weather(request.args).to_json()
 
 
 @app.route('/api/v1.0/forecast')
+@crossdomain(origin='*')
 def forecast():
     return node.get_forecast_weather(request.args).to_json()
 
 
 @app.route('/loadfile/<filename>')
+@crossdomain(origin='*')
 def uploaded_file(filename):
     image = send_from_directory(app.config['UPLOAD_FOLDER'], filename)
     return image
